fb3e8aec13a8b72a94345a1b94d2831a3f953ae3
Vic Cherubini <vcherubini@gmail.com>
2025-05-17 12:05:18 -0500

    more tests

--- src/Request/RasterizeFileRequest.php
+++ src/Request/RasterizeFileRequest.php
@@ -9,12 +9,29 @@ use function is_readable;
 
 final readonly class RasterizeFileRequest
 {
+    private const int MIN_RESOLUTION = 48;
+    private const int MAX_RESOLUTION = 300;
+
     public function __construct(
         public string $filePath,
-        public int $resolution = 300,
+        public int $pageNumber = 1,
+        public int $pageCount = 1,
+        public int $resolution = 150,
     ) {
         if (!is_file($this->filePath) || !is_readable($this->filePath)) {
             throw new InvalidArgumentException(sprintf('The input file "%s" does not exist or is not readable.', $this->filePath));
+        }
+
+        if ($this->pageNumber < 1) {
+            throw new InvalidArgumentException('The page number must be an integer greater than 0.');
+        }
+
+        if ($this->pageCount < 0) {
+            throw new InvalidArgumentException('The page count must be an integer greater than or equal to 0.');
+        }
+
+        if ($this->resolution < self::MIN_RESOLUTION || $this->resolution > self::MAX_RESOLUTION) {
+            throw new InvalidArgumentException(sprintf('The resolution must be an integer between %d and %d.', self::MIN_RESOLUTION, self::MAX_RESOLUTION));
         }
     }
 }
--- tests/Request/RasterizeFileRequestTest.php
+++ tests/Request/RasterizeFileRequestTest.php
@@ -7,6 +7,10 @@ use OneToMany\PdfToImage\Request\RasterizeFileRequest;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 
+use function random_int;
+
+use const PHP_INT_MAX;
+
 #[Group('UnitTests')]
 #[Group('RequestTests')]
 final class RasterizeFileRequestTest extends TestCase
@@ -19,5 +23,57 @@ final class RasterizeFileRequestTest extends TestCase
         $this->expectExceptionMessage('The input file "'.$filePath.'" does not exist or is not readable.');
 
         new RasterizeFileRequest($filePath);
+    }
+
+    public function testConstructorRequiresPositivePageNumber(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The page number must be an integer greater than 0.');
+
+        new RasterizeFileRequest(__FILE__, 0);
+    }
+
+    public function testConstructorRequiresPositiveOrZeroPageCount(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The page count must be an integer greater than or equal to 0.');
+
+        new RasterizeFileRequest(__FILE__, 1, -1);
+    }
+
+    public function testConstructorRequiresResolutionToBeLessThanOrEqualToMinimumResolution(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The resolution must be an integer between 48 and 300.');
+
+        new RasterizeFileRequest(__FILE__, 1, 1, random_int(1, 47));
+    }
+
+    public function testConstructorRequiresResolutionToBeLessThanOrEqualToMaximumResolution(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The resolution must be an integer between 48 and 300.');
+
+        new RasterizeFileRequest(__FILE__, 1, 1, random_int(301, PHP_INT_MAX));
+    }
+
+    public function testConstructor(): void
+    {
+        $filePath = __FILE__;
+        $pageNumber = random_int(1, 100);
+        $pageCount = random_int(0, 100);
+        $resolution = random_int(48, 300);
+
+        $request = new RasterizeFileRequest(...[
+            'filePath' => $filePath,
+            'pageNumber' => $pageNumber,
+            'pageCount' => $pageCount,
+            'resolution' => $resolution,
+        ]);
+
+        $this->assertEquals($filePath, $request->filePath);
+        $this->assertEquals($pageNumber, $request->pageNumber);
+        $this->assertEquals($pageCount, $request->pageCount);
+        $this->assertEquals($filePath, $request->filePath);
     }
 }
