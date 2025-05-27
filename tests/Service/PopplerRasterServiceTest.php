6449e7bd26c555ae8999f6721e662d1b8aac7425
Vic Cherubini <vcherubini@gmail.com>
2025-05-27 17:26:48 -0500

    wip

--- tests/Service/PopplerRasterServiceTest.php
+++ tests/Service/PopplerRasterServiceTest.php
@@ -59,10 +59,7 @@ final class PopplerRasterServiceTest extends TestCase
         ImageType $type,
         int $resolution,
         string $sha1,
-    ): void
-    {
-        $file = __DIR__.'/files/'.$file;
-
+    ): void {
         $request = new RasterizeFileRequest(
             $file, $page, $type, $resolution
         );
@@ -77,11 +74,11 @@ final class PopplerRasterServiceTest extends TestCase
     public static function providerFilePageTypeResolutionAndSha1Hash(): array
     {
         $provider = [
-            ['pages-1.pdf', 1, ImageType::Jpeg, 150, 'bfbfea39b881befa7e0af249f4fff08592d1ff56'],
-            ['pages-2.pdf', 1, ImageType::Jpeg, 300, 'f8b755881dc51980e8a9b4bb147a9c1388f91768'],
-            ['pages-2.pdf', 1, ImageType::Png, 150, 'ba1276ebf1e1cbd934e3f9b54a6659678ad4f918'],
-            ['pages-3.pdf', 1, ImageType::Jpeg, 72, '5ff2aaa08133b6129371a3f61d96c1522626c974'],
-            ['pages-4.pdf', 1, ImageType::Png, 72, 'cd49f354f52895745b94845bfd7261e01a5458d9'],
+            [__DIR__.'/files/pages-1.pdf', 1, ImageType::Jpeg, 150, 'bfbfea39b881befa7e0af249f4fff08592d1ff56'],
+            [__DIR__.'/files/pages-2.pdf', 1, ImageType::Jpeg, 300, 'f8b755881dc51980e8a9b4bb147a9c1388f91768'],
+            [__DIR__.'/files/pages-2.pdf', 1, ImageType::Png, 150, 'ba1276ebf1e1cbd934e3f9b54a6659678ad4f918'],
+            [__DIR__.'/files/pages-3.pdf', 1, ImageType::Jpeg, 72, '5ff2aaa08133b6129371a3f61d96c1522626c974'],
+            [__DIR__.'/files/pages-4.pdf', 1, ImageType::Png, 72, 'cd49f354f52895745b94845bfd7261e01a5458d9'],
         ];
 
         return $provider;
