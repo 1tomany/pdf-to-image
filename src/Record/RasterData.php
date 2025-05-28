fd293e19e851f49000eebf0cf9d8fd6b2d34dcc7
Vic Cherubini <vcherubini@gmail.com>
2025-05-28 15:40:46 -0500

    wip

--- src/Record/RasterData.php
+++ src/Record/RasterData.php
@@ -2,6 +2,9 @@
 
 namespace OneToMany\PdfToImage\Record;
 
+use OneToMany\DataUri\SmartFile;
+use OneToMany\PdfToImage\Exception\RuntimeException;
+
 use function base64_encode;
 use function sprintf;
 
@@ -21,5 +24,14 @@ final readonly class RasterData implements \Stringable
     public function toDataUri(): string
     {
         return sprintf('data:%s;base64,%s', $this->contentType, base64_encode($this->bytes));
+    }
+
+    public function toSmartFile(): SmartFile // @phpstan-ignore-line
+    {
+        if (!\class_exists(SmartFile::class)) {
+            throw new RuntimeException('The raster data can not be converted to a SmartFile because the library is not installed. Try running "composer require 1tomany/data-uri".');
+        }
+
+        return \OneToMany\DataUri\parse_data($this->toDataUri()); // @phpstan-ignore-line
     }
 }
