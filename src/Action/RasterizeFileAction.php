92b47814d3ef0d095f6ff9c295cafcdffdd8d1c8
Vic Cherubini <vcherubini@gmail.com>
2025-05-29 16:37:25 -0500

    wip

--- src/Action/RasterizeFileAction.php
+++ src/Action/RasterizeFileAction.php
@@ -4,12 +4,16 @@ namespace OneToMany\PdfToImage\Action;
 
 use OneToMany\PdfToImage\Record\RasterImage;
 use OneToMany\PdfToImage\Request\RasterizeFileRequest;
+use OneToMany\PdfToImage\Service\PdfInfoService;
 use OneToMany\PdfToImage\Service\RasterServiceInterface;
 
 final readonly class RasterizeFileAction
 {
+    private PdfInfoService $pdfInfoService;
+
     public function __construct(private RasterServiceInterface $rasterService)
     {
+        $this->pdfInfoService = new PdfInfoService();
     }
 
     /**
@@ -17,6 +21,15 @@ final readonly class RasterizeFileAction
      */
     public function act(RasterizeFileRequest $request): array
     {
+        // Validate the Request
+        $pdfInfo = $this->pdfInfoService->read(...[
+            'filePath' => $request->filePath,
+        ]);
+
+        if ($request->finalPage > $pdfInfo->pageCount) {
+            throw new \RuntimeException('final page too large');
+        }
+
         return $this->rasterService->rasterize($request);
     }
 }
